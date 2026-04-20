f579b75bdf6507aaaf9a6a9df2fc9d1789a1eea4
Dabsia <dabojohnson98@gmail.com>
2026-04-21 00:51:48 +0100

    maade some channges

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -1,10 +1,9 @@
 import express from 'express'
 import {
-    createOrder,
     getOrders,
     getOrderById,
     getOrdersByUser,
-    updateOrder,
+    // updateOrder,
     deleteOrder,
     getOrderStats
 } from '../controller/order.controller.js'
@@ -12,12 +11,12 @@ import { protect } from '../middleware/index.js'
 
 const router = express.Router()
 
-router.post('/', protect, createOrder)
+// router.post('/', protect, createOrder)
 router.get('/', protect, getOrders)
 router.get("/stats", getOrderStats);
 router.get('/:id', protect, getOrderById)
 router.get('/user/:userId', protect, getOrdersByUser)
-router.put('/:id', protect, updateOrder)
+// router.put('/:id', protect, updateOrder)
 router.delete('/:id', protect, deleteOrder)
 
 export default router
