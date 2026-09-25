259ca3c87f02f6d7ccdd280544936300cfce4d66
Dabsia <dabojohnson98@gmail.com>
2026-09-25 13:17:51 +0100

    made some updates

--- services/email.js
+++ services/email.js
@@ -5,7 +5,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 export const sendEmail = ({ to, subject, html, from, phoneNumber }) => {
     resend.emails.send({
       from: 'onboarding@resend.dev',
-      to: to || 'daultimatestores@gmail.com',
+      to: to || 'dabojohnson98@gmail.com',
       title: {subject},
       subject: subject,
       phoneNumber: phoneNumber,
